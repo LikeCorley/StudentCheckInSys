217b018a8639201f3d30cb910c7f3eb960157b57
zyt1378 <30614986+zyt1378@users.noreply.github.com>
2018-05-11 13:22:03 +0800

    学生签到系统移动端
    完成登录页面界面设计

--- 移动端/学生签到系统移动端/src/app/app.module.ts
+++ 移动端/学生签到系统移动端/src/app/app.module.ts
@@ -3,21 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import {LoginPage} from "../pages/login/login";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    LoginPage
   ],
   providers: [
     StatusBar,
